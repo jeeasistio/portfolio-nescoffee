3c71158c065e8e12ce7cb1c73fa56ef8b3676c86
JeeAsistio <67195225+jeeasistio@users.noreply.github.com>
2021-12-09 13:18:29 +0800

    Update _document.js
    font fix

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,10 @@ class MyDocument extends Document {
           href="https://fonts.googleapis.com/css2?family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&display=swap"
           rel="stylesheet"
         />
-        <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,200;0,400;0,500;0,600;0,800;1,200;1,500;1,700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,200;0,400;0,500;0,600;0,800;1,200;1,500;1,700&display=swap"
+          rel="stylesheet"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
