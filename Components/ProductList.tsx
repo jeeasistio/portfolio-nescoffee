e29d8141a9ce726124f423d10e5ac1ff4b8820cc
JeeAsistio <67195225+jeeasistio@users.noreply.github.com>
2021-12-09 13:26:46 +0800

    Update ProductList.tsx
    margin fix

--- Components/ProductList.tsx
+++ Components/ProductList.tsx
@@ -12,7 +12,7 @@ interface Props extends ProductList {}
 const sx: SxProps = {
   root: {
     my: 4,
-    mb: 12
+    mb: 8
   },
   categoryCtn: {
     width: { xs: 0.5, sm: 0.4, md: 0.3, lg: 0.2, xl: 0.1 },
