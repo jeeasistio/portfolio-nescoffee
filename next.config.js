b1a8925c570496638f1b1c252f8fb1d582e7e6f2
jeeasistio <jeeasistio08@gmail.com>
2022-03-18 12:05:17 +0800

    env added

--- next.config.js
+++ next.config.js
@@ -4,13 +4,6 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true
   },
-  env: {
-    MONGO_URI:
-      'MONGO_URI=mongodb+srv://jeeasistio:jeeasistio@cluster0.et8sn.mongodb.net/coffee?retryWrites=true&w=majority',
-    GOOGLE_USER: 'nescoffee.app@gmail.com',
-    GOOGLE_PASS: 'nescoffee21232',
-    ADMIN_USER: 'jeeasistio08@gmail.com'
-  },
   images: {
     domains: ['i.ibb.co', 'source.unsplash.com']
   }
